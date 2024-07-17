bc0f8f53fb82b3786308045b8fa9affae5a1d003
limonadaweb <adrianafernandezlagoa@gmail.com>
2024-07-17 18:41:44 +0200

    https://github.com/FemCoders-Norte/variables-javascript-nivel-3-limonadaweb.git

--- src/basic.js
+++ src/basic.js
@@ -1,13 +1,25 @@
+//Escribe tu código aquí
+
 function welcome(){
-    //Escribe tu código aquí
+    console.log("¡Bienvenido!");
 };
-
+welcome();
+//Escribe tu código aquí
 function evenOrOdd(n){
-    //Escribe tu código aquí
-};
-
+    
+    if (n % 2 === 0) {
+        return 'par';
+    } else {
+        return 'impar';
+    }
+}
+console.log(evenOrOdd(5)); // Imprime 'impar'
+console.log(evenOrOdd(10)); // Imprime 'par'
+console.log(evenOrOdd(0)); // Imprime 'par'
+//Escribe tu código aquí
 function greeting(name) {
-    //Escribe tu código aquí
-};
+    console.log(`¡Hola, ${name}! Bienvenido.`);
+}
+greeting('Pedrito');
 
 export { evenOrOdd, greeting, welcome }
